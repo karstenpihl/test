977a136f3756051c6b9a7aa36f1f3e4fe24a2f2a
karstenpihl <karsten@septima.dk>
2013-12-20 15:52:22 +0100

    Test

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ['dist'],
+    concat: {
+      options: {
+        separator: ';',
+        banner: '/* Septima Widget - VERSION <%= pkg.version %> - build <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        src: ['src/test*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    'gh-pages': {
+      options: {
+        base: 'dist',
+      },
+      'gh-pages': {
+        options: {
+          tag: '<%= pkg.version %>',
+          message: 'Auto-generated commit. Version <%= pkg.version %>'
+        },
+        src: ['**']
+      },
+      'test': {
+        options: {
+          branch: 'test',
+          message: 'Test Version <%= pkg.version %>'
+        },
+        src: ['**']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+
+  grunt.registerTask('default', ['concat','gh-pages:test']);
+
+  grunt.registerTask('publish', ['concat','gh-pages:gh-pages']);
+  
+};
