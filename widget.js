05800d7887e41aa1b13eef7c6c838780bd026d6d
karstenpihl <karsten@septima.dk>
2013-12-23 14:38:30 +0100

    Auto-generated commit. Version 1.1.3

--- widget.js
+++ widget.js
@@ -1,6 +1,6 @@
-/* Septima Widget - VERSION 1.1.2 - build 23-12-2013 */
+/* Septima Widget - VERSION 1.1.3 - build 23-12-2013 */
 function test1 () {
-    alert(123);
+    alert(1232);
 };function test2 () {
     alert(456);
 }
